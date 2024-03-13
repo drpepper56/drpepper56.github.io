e86c76019b9860eef52ceb6c1ebfccab163425cb
Karol Bura <karol.bura544@gmail.com>
2024-03-13 13:42:03 +0000

    fixed build path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './build', // Specify the directory for the production build
+  },
   plugins: [react()],
 })
